3a8ecd529579937fbd96c6407694dbe9fc357202
Kabita09-gif <kabitaroy120@gmail.com>
2026-06-25 12:51:00 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        //checking rowwise
+        for(int row=0;row<9;row++){
+            unordered_set<char>seen;
+            for(int j=0;j<9;j++){
+              if(board[row][j]=='.') continue;
+              if(seen.count(board[row][j])) return false;
+
+              seen.insert(board[row][j]);
+            }
+        }
+
+        //checking columnwise
+        for(int col=0;col<9;col++){
+            unordered_set<char>seen;
+            for(int i=0;i<9;i++){
+                if(board[i][col]=='.') continue;
+                if(seen.count(board[i][col])) return false;
+
+                seen.insert(board[i][col]);
+            }
+        }
+        //checking 9X9 grid
+        for(int square=0;square<9;square++){
+            unordered_set<char>seen;
+
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    int row=(square/3)*3 +i;
+                    int col=(square%3)*3+j;
+
+                    if(board[row][col]=='.')  continue;
+                    if(seen.count(board[row][col]))  return false;
+
+                    seen.insert(board[row][col]);
+                }
+            }
+        }
+        return true;
+    }
+};
