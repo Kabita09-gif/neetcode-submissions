46131581fa6b4c17e4d9aaa0b5aed112d5ffbc56
Kabita09-gif <kabitaroy120@gmail.com>
2026-07-11 22:12:19 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    set<vector<int>>s;
+    void solve(vector<int>nums, vector<int>ans){
+      if(nums.size()==0){
+        s.insert(ans);
+        return;
+      }
+
+      vector<int>ans1=ans; //include
+      vector<int>ans2=ans;  //exclude 
+
+      ans1.push_back(nums[0]); //including the element
+      nums.erase(nums.begin()); //smaller input 
+
+      solve(nums,ans1);
+      solve(nums,ans2);
+
+      return;
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>ans;
+        solve(nums,ans);
+        vector<vector<int>>output(s.begin(),s.end());
+        return output;
+    }
+};
