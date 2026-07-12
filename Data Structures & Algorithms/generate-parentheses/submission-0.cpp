3b7be972e849117711708ceddd81ea0e9bff26ed
Kabita09-gif <kabitaroy120@gmail.com>
2026-07-12 17:00:45 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(int open,int close,string op,vector<string>&ans){
+        if(open==0 && close==0){
+            ans.push_back(op);
+            return ;
+        }
+
+        if(open>0){
+            string op1=op;
+            op1.push_back('(');
+            solve(open-1,close,op1,ans);
+        }
+
+        if(open<close){
+            string op2=op;
+            op2.push_back(')');
+            solve(open,close-1,op2,ans);
+        }
+        return;
+    }
+    vector<string> generateParenthesis(int n) {
+        int open=n;
+        int close=n;
+
+        vector<string>ans;
+        string op="";
+        solve(open,close,op,ans);
+        return ans;
+    }
+};
